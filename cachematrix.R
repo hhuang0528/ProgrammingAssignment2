92281dc22f088aa32e453eefdf80216c548503da
hhuang0528 <gracehuanghuang86@gmail.com>
2016-11-06 02:41:34 -0600

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overal there are 2 functions, 1st function makeCachMatrix generate a list
+## store calculated inverse of matrix and 2nd function to compute inverse of
+## matrix but retrived stored inverse in cach from makeCachMatrix
 
-## Write a short comment describing this function
+## Creat a list to store inputmatrix and inverse matrix calcuated
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCachMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## retrieve inverse stored in catche if it's calculated before,
+##otherwise compute the inverse of geiven square matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached inverse")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
